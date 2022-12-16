e05066c86a8a2922be7bd93eac8b0cf481c04f4c
Konstantin <hi@enflow.io>
2022-12-16 19:10:10 +0300

    Main screen brands list

--- components/Features/MainPage/BrandsList/BrandsList.tsx
+++ components/Features/MainPage/BrandsList/BrandsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Brand } from '../../../../interfaces/brand.interface';
 import classes from './BrandsList.module.scss';
 
@@ -6,10 +6,21 @@ export interface BrandsListProps {
 
 }
 
+const ITEMS_QNT_SHORT_MODE = 12;
+
 const BrandsList = ({ }: BrandsListProps) => {
+  const [isShortMode, setIsShortMode] = useState(true)
+
+  const toggleMode = (e: Event) => {
+    e.preventDefault();
+    setIsShortMode(!isShortMode);
+  }
+
+  const data = isShortMode ? BrandsListData.slice(0, ITEMS_QNT_SHORT_MODE) : BrandsListData;
+
   return <div className={classes.BrandsList}>
     <ul>
-      {BrandsListData.map(el => {
+      {data.map(el => {
         return <li key={el.name}>
           <a href=''>
             <img width={30} src={el.imgUrl} /><span>{el.name}</span>
@@ -17,12 +28,72 @@ const BrandsList = ({ }: BrandsListProps) => {
         </li>
       })}
     </ul>
-    <a className={classes.BrandLink} href='#'>Показать все марки</a>
+    <a
+      onClick={toggleMode}
+      className={classes.BrandLink}
+      href='#'>{isShortMode ? "Показать все марки" : "Показать меньше"}
+    </a>
   </div>;
 };
 
 
 const BrandsListData: Brand[] = [
+  {
+    imgUrl: "/img/brands/audi.png",
+    name: "Audi"
+  },
+  {
+    imgUrl: "/img/brands/bmw.jpg",
+    name: "BMW"
+  },
+  {
+    imgUrl: "/img/brands/chevrolet.png",
+    name: "Chevrolet"
+  },
+  {
+    name: "Ford",
+    imgUrl: "/img/brands/ford.png"
+  },
+  {
+    name: "Hyndai",
+    imgUrl: "/img/brands/hunday.png"
+  },
+  {
+    name: "Kia",
+    imgUrl: "/img/brands/kia.png"
+  },
+  {
+    name: "LADA",
+    imgUrl: "/img/brands/lada.png"
+  },
+  {
+    imgUrl: "/img/brands/merc.png",
+    name: "Mersedes-Benz"
+  },
+  {
+    name: "Mitsubishi",
+    imgUrl: "/img/brands/mitsubishi.png"
+  },
+  {
+    name: "Nissan",
+    imgUrl: "/img/brands/nissan.png"
+  },
+  {
+    name: "Renault",
+    imgUrl: "/img/brands/renault.png"
+  },
+  {
+    name: "Skoda",
+    imgUrl: "/img/brands/skoda.png"
+  },
+  {
+    imgUrl: "/img/brands/tayota.png",
+    name: "Toyota"
+  },
+  {
+    imgUrl: "/img/brands/vw.png",
+    name: "Volkswagen"
+  },
   {
     imgUrl: "/img/brands/audi.png",
     name: "Audi"
